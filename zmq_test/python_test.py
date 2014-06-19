c2f57b3e1dfb8e446b3ca73cdb92b0554913b237
Nirlendu <nirlendu@gmail.com>
2014-06-19 14:41:18 +0530

    timeout added

--- zmq_test/python_test.py
+++ zmq_test/python_test.py
@@ -3,6 +3,6 @@ s = requests.Session()
 a = requests.adapters.ZMQAdapter()
 a.pattern='req_rep'
 s.mount('tcp://', a)
-x = s.get("tcp://127.0.0.1:5678",data='hey man first !')
+x = s.get("tcp://127.0.0.1:5678",data='check the code!',timeout=1)
 print x.status_code
 print x.content
